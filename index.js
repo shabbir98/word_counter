298d89befce5076e650ea6cd0cc0bdb5bc9702f6
shabbir98 <84226838+shabbir98@users.noreply.github.com>
2023-09-27 01:42:31 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer';
+function countWordsAndCharacters(input) {
+    const words = input.split(/\s+/).filter((word) => word !== '').length;
+    const characters = input.replace(/\s+/g, '').length;
+    return { words, characters };
+}
+const questions = [
+    {
+        message: 'Enter a paragraph:',
+        type: 'input',
+        name: 'paragraph',
+    },
+];
+let exitOrContinue = '';
+do {
+    await inquirer.prompt(questions).then((answers) => {
+        const { words, characters } = countWordsAndCharacters(answers.paragraph);
+        console.log(`Word count (without whitespace): ${words}\nCharacter count (without whitespace): ${characters}`);
+    });
+    const askAgain = await inquirer.prompt({
+        message: 'Press "x" for exit or any key to continue:',
+        type: 'input',
+        name: 'x_or_y',
+    });
+    exitOrContinue = askAgain.x_or_y;
+    console.clear();
+} while (exitOrContinue !== 'x');
